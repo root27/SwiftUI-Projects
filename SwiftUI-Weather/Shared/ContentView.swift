747499882aaec3c7bc7ef56688c50e3638fe4732
root27 <oguzhaneee@gmail.com>
2023-01-21 14:12:15 +0300

    Weather App

---
+++ SwiftUI-Weather/Shared/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by OGUZHAN DOGAN on 20.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Izmir")
+                    .font(.system(size:32,weight: .medium,design:.default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing:8){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:180,height:180)
+                    Text("30°")
+                        .foregroundColor(.white)
+                        .font(.system(size:60,weight:.medium))
+                    
+                        .padding(.bottom,40)
+                    
+                    HStack(spacing:20){
+                        WeatherofDay(dayofweek: "TUE", imageName: "cloud.sun.fill", degree: 25)
+                        
+                        WeatherofDay(dayofweek: "WED", imageName: "cloud.sun.rain.fill", degree: 20)
+                        
+                        WeatherofDay(dayofweek: "THU", imageName: "cloud.sun.bolt.fill", degree: 18)
+                        
+                        WeatherofDay(dayofweek: "FR", imageName: "cloud.rain.fill", degree: 15)
+                        
+                        WeatherofDay(dayofweek: "SAT", imageName: "cloud.bolt.rain.fill", degree: 16)
+                    }
+                    Spacer()
+                    
+                    Button{
+                        print("tapped")
+                    } label: {
+                        Text("Change Day Time")
+                            .frame(width:280, height:50)
+                            .background(.white)
+                            .font(.system(size:20,weight:.bold, design: .default))
+                            .cornerRadius(20)
+                    }
+                    Spacer()
+                }
+                    
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct WeatherofDay: View {
+    
+    var dayofweek: String
+    var imageName: String
+    var degree: Int
+    
+    var body: some View {
+        
+       
+        
+        VStack{
+            Text(dayofweek)
+                .foregroundColor(.white)
+                .font(.system(size:16,weight:.medium))
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:40,height:40)
+            
+            Text("\(degree)°")
+                .foregroundColor(.white)
+                .font(.system(size:28,weight:.medium))
+        }
+    }
+}
---
+++ SwiftUI-Weather/Shared/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  Shared
+//
+//  Created by OGUZHAN DOGAN on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
